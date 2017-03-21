38b02554e814b090ef3c88a6b5aa29b007e82e2e
NOOR <noor.s.suliman@gmail.com>
2017-03-21 20:37:03 +0300

    adding images and icons

--- src/App.js
+++ src/App.js
@@ -4,17 +4,10 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div id = "sidebar">
+      <p><a href=""><img class="logo" src="./src/images/profile.jpg"></img></a></p> 
+      <h1>heloo</h1>
+    </div>
   }
 }
 
