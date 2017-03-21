667ddbd4063e16025d707d118f9a2b0052898475
NOOR <noor.s.suliman@gmail.com>
2017-03-21 22:02:51 +0300

    installing react-router package and added title and tagline plus nav menue

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ class App extends Component {
     return <div id="sidebar">
       <p><a href=""><img className='logo' src={propic} alt="noor"/></a></p> 
       <h1 id="blogtitle">Noor Sulaiman</h1>
+      <p className='tagline'>I do Cook I do Code</p>
+      <div ClassName='menu'>
+        <ul>
+          <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
+          <li><Link to="/about" activeClassName="active">About</Link></li>
+          <li><Link to="/projects" activeClassName="active">Projects</Link></li>
+          <li><Link to="/Skills" activeClassName="active">Skills</Link></li>
+        </ul>
+      </div>
     </div>
   }
 }
