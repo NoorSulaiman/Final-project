101b7b5b9923982e3336d20ec8d193224313f695
NOOR <noor.s.suliman@gmail.com>
2017-03-25 16:58:12 +0300

    updating home component and adding styles to it

--- src/components/main/home/home.js
+++ src/components/main/home/home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import './home.css'
 
 
 class Home extends Component{
 
 	render(){
 
-		return <h1> Hello </h1>
+		return<div className='home'>
+		 <h1> Welcome to my portfolio web app </h1>
+		 <h3>This website has been built with React</h3>
+		 </div>
 	}
 
 }
