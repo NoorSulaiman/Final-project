e77321b0911cea72d304e680a6b86057c9990daf
NOOR <noor.s.suliman@gmail.com>
2017-03-25 19:17:10 +0300

    updating home component and style

--- src/components/main/home/home.js
+++ src/components/main/home/home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './home.css'
 
+import logo from '../../../logo.svg';
+
 
 class Home extends Component{
 
@@ -9,6 +11,8 @@ class Home extends Component{
 		return<div className='home'>
 		 <h1> Welcome to my portfolio web app </h1>
 		 <h3>This website has been built with React</h3>
+		 <img src={logo} className="App-logo" alt="logo" />
+		 <h4> To know more about me please navigate through the app using the sidebar navifation</h4>
 		 </div>
 	}
 
