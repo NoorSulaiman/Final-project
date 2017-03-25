73798566533f15b960d8b304a51e7034b1b26f41
NOOR <noor.s.suliman@gmail.com>
2017-03-25 23:26:39 +0300

    updating home component and adding animations to the styles

--- src/components/main/home/home.js
+++ src/components/main/home/home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './home.css'
 
 import logo from '../../../logo.svg';
-
+import arrow from '../../../images/arrow.png';
 
 class Home extends Component{
 
@@ -12,10 +12,13 @@ class Home extends Component{
 		 <h1> Welcome to my portfolio web app </h1>
 		 <h3>This website has been built with React</h3>
 		 <img src={logo} className="App-logo" alt="logo" />
-		 <h4> To know more about me please navigate through the app using the sidebar navifation</h4>
+		 <h4> To know more about me please navigate through the app using the sidebar navigations</h4>
+		<img src={arrow} className='Arrow' alt='arrow'/>
 		 </div>
-	}
+		}
+		}
+	
 
-}
+
 
 export default Home;
