d37090a010c9f1aa42594ed96b1a7bea39fc4b44
Noor Sulaiman <noor.s.sulaiman@gmail.com>
2020-02-04 21:18:20 -0500

    adding favicon fixing some styles

--- src/components/display/skills/data.js
+++ src/components/display/skills/data.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 
-
-
-var data = require('./data.json');
+const data = require('./data.json');
 
 class Data extends Component {
-
-
         render() {
-
-                var i = 0;
-                var mySkills = _.map(data, (skills) => {
-                        i++
-                        return <div className={`skill scol${i}`} key={Math.random()}>
-                                <img src={require(skills.img)} alt={skills.name} />
-                                <h3>{skills.name}</h3>
-                                <div className='bar'>
-                                        <div id={`progress${i}`}></div>
+                let i = 0;
+                const mySkills = _.map(data, skills => {
+                        i++;
+                        return (
+                                <div className={`skill scol${i}`} key={Math.random()}>
+                                        <div className="skilliconWrapper">
+                                                <img src={require(skills.img)} alt={skills.name} />
+                                                <h3>{skills.name}</h3>
+                                        </div>
+                                        <div className="bar">
+                                                <div id={`progress${i}`} />
+                                        </div>
                                 </div>
-                        </div>
-                })
+                        );
+                });
 
-                return <span className='skillswraper'>
-                        {mySkills}
-                </span>
-
-
+                return <span className="skillswraper">{mySkills}</span>;
         }
 }
 
